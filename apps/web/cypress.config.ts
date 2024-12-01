cc21c6bae53fd84f75832fdbf48073b867ccd7d1
jonathan <jonathanpmelian@gmail.com>
2024-12-01 14:52:46 +0000

    feat(auth): app rdockerized dev and test environments

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    // Testing environment
+    baseUrl: "http://localhost:3100",
     supportFile: false,
     specPattern: "tests/e2e/**/*.cy.{ts,tsx}",
   },
