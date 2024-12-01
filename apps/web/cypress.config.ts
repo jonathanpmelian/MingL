55d9d27213867fa54e84d4b943287ca03abbac90
jonathan <jonathanpmelian@gmail.com>
2024-12-01 17:12:00 +0000

    feat(auth): e2e testing environment running on docker

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     // Testing environment
-    baseUrl: "http://localhost:3100",
+    baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:3100",
     supportFile: false,
     specPattern: "tests/e2e/**/*.cy.{ts,tsx}",
   },
